bae59e0e7f609a026c115c91553a8bbf8db26c64
Kidus <km3533@columbia.edu>
2022-12-10 17:09:40 -0500

    added calls to API for getting attendances and questions

--- professor.js
+++ professor.js
@@ -1,17 +1,15 @@
 
-const questions = [
-    {
-        "question" : "What does the 'o' in IoT stand for?",
-        "votes" : 12,
-        "user" : "km3533"
+const questions = {
+    "q1key" : {
+        "question_text" : "What does the 'o' in IoT stand for?",
+        "student_id" : "km3533"
     },
-    {
-        "question" : "What is the worst development board, and why is it the ESP8266?",
-        "votes" : 21,
-        "user" : "km3533"
+    "q2key" : {
+        "question_text" : "What is the worst development board, and why is it the ESP8266?",
+        "student_id" : "km3533"
 
     }
-]
+}
 
 const attendances = {
     "km3533" : 0,
@@ -19,15 +17,19 @@ const attendances = {
     "sd2124" : 1
 }
 
+let class_id = 'IoT'
+
+const api_endpoint = "https://tiaa5tbuqi.execute-api.us-east-1.amazonaws.com/v1"
+
 function make_question(question) {
     const card = $("<div class='row question-container'>")
     const question_user = $("<div class='col-3 question-user'>")
     const question_text = $("<div class='col-8 question-text'>")
     const question_votes = $("<div class='col-1 question-votes'>")
 
-    question_user.html(question.user)
-    question_text.html(question.question)
-    question_votes.html(question.votes)
+    question_user.html(question.student_id)
+    question_text.html(question.question_text)
+    question_votes.html("")
 
     card.append(question_user, question_text, question_votes)
 
@@ -48,9 +50,56 @@ function fill_attendances(attendances) {
     })
 }
 
+function get_attendances() {
+    const attendances_endpoint = api_endpoint + "/get_attendances"
+    const args = { "class_id" : class_id }
+    $.ajax({
+        type: "GET",
+        url: attendances_endpoint,
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: args,
+        success: function(result) {
+            console.log("Got results", result)
+            fill_attendances(result)
+        },
+        error: function(request, status, error) {
+            console.log("Error: ")
+            console.log(request)
+            console.log(status)
+            console.log(error)
+        }
+    })
+}
+
+function get_questions() {
+    const questions_endpoint = api_endpoint + "/get_questions"
+    const args = { "class_id" : class_id }
+    $.ajax({
+        type: "GET",
+        url: questions_endpoint,
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: args,
+        success: function(result) {
+            console.log("Got results", result)
+            fill_questions(result)
+        },
+        error: function(request, status, error) {
+            console.log("Error: ")
+            console.log(request)
+            console.log(status)
+            console.log(error)
+        }
+    })
+}
+
 $(document).ready(function (){
     console.log("Document ready")
 
     fill_questions(questions)
     fill_attendances(attendances)
+
+    get_attendances()
+    get_questions()
 })
